421d2659895c0606f423f11942f71ca94da5dcc5
Robeszpierre <robeszpierre@gmail.com>
2016-06-06 23:15:19 +0200

    footer social icons

--- js/js.js
+++ js/js.js
@@ -3,3 +3,15 @@ $( ".logo" ).hover(function(){
 }, function(){
   $(".mailCaption").css( "visibility", "hidden" );
 });
+
+$(".icon-mark-github").hover(function(){
+  $(this).css("background-position", "0 -64px");
+}, function() {
+  $(this).css("background-position", "0 0px");
+});
+
+$(".icon-facebook").hover(function(){
+  $(this).css("background-position", "0 -96px");
+}, function() {
+  $(this).css("background-position", "0 -32px");
+});
